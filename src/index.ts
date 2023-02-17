6bd800a83ffbc5a312f5249de46d140400240e8a
Chesney Nyffels <chesney@nyffels.be>
2023-02-17 13:08:36 +0100

    Enabled authorization

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,6 @@ app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.static('public'));
 
-// app.use((req, res, next) => {
-// 	console.log(req.headers.authorization);
-// 	if (process.env.CODE != req.headers.authorization) throw new Error("Incorrect api key. More information: https://github.com/Nyffels-IT/HTML-to-PDF-Socker-Swagger-API");
-// 	next();
-// });
-
-app.use(Router);
-
 app.use(
   "/documentation",
   swaggerUi.serve,
@@ -32,6 +24,14 @@ app.use(
     },
   })
 );
+
+app.use((req, res, next) => {
+	console.log(req.headers.authorization);
+	if (process.env.CODE != req.headers.authorization) throw new Error("Incorrect api key. More information: https://github.com/Nyffels-IT/HTML-to-PDF-Socker-Swagger-API");
+	next();
+});
+
+app.use(Router);
 
 app.listen(PORT, () => {
 	console.log('Server is running on port', PORT);
